985cbe5b09ba89201e924b43c5534cbf5557c08e
Vladimir Moshchuk <ardonplay@gmail.com>
2023-12-02 22:43:44 +0300

    feat: add likeToGenreAgent

--- src/main/java/io/github/semanticpie/orchestrator/OrchestratorApplication.java
+++ src/main/java/io/github/semanticpie/orchestrator/OrchestratorApplication.java
@@ -3,14 +3,10 @@ package io.github.semanticpie.orchestrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class OrchestratorApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(OrchestratorApplication.class, args);
     }
 
